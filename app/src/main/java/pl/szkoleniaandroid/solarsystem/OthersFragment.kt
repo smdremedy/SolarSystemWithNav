438f492ab8e9b23da1704fb2dbbd71b9b73cb024
Sylwester Madej <sylwek.madej@gmail.com>
2019-10-22 00:08:59 +0200

    Let's start from here

--- app/src/main/java/pl/szkoleniaandroid/solarsystem/OthersFragment.kt
+++
@@ -1,6 +0,0 @@
-package pl.szkoleniaandroid.solarsystem
-
-class OthersFragment : SolarObjectsFragment() {
-
-    override fun createObjects() = repository.others
-}
--- app/src/main/java/pl/szkoleniaandroid/solarsystem/SolarObjectsAdapter.kt
+++ app/src/main/java/pl/szkoleniaandroid/solarsystem/SolarObjectsAdapter.kt
@@ -1,51 +1,12 @@
 package pl.szkoleniaandroid.solarsystem
 
-import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
-import kotlinx.android.synthetic.main.solar_objects_fragment.*
-
-abstract class SolarObjectsFragment : Fragment() {
-
-    private lateinit var solarObjectsAdapter: SolarObjectsAdapter
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.solar_objects_fragment, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        solarObjectsAdapter = SolarObjectsAdapter()
-        solarObjectsAdapter.setObjects(createObjects())
-        solarObjectsAdapter.objectClickedListener = object : ObjectClickedListener {
-            override fun objectClicked(clickedObject: SolarObject) {
-                val args = ObjectDetailsFragmentArgs.Builder(clickedObject).build()
-                findNavController().navigate(R.id.nav_details, args.toBundle())
-            }
-        }
-        objectsRecyclerView.adapter = solarObjectsAdapter
-        objectsRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        solarObjectsAdapter.objectClickedListener = null
-    }
-
-    abstract fun createObjects(): Array<SolarObject>
-}
 
 class SolarObjectsAdapter : RecyclerView.Adapter<SolarObjectsViewHolder>(),
     ObjectClickedListener {
@@ -56,7 +17,8 @@ class SolarObjectsAdapter : RecyclerView.Adapter<SolarObjectsViewHolder>(),
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SolarObjectsViewHolder {
         val itemView =
-            LayoutInflater.from(parent.context).inflate(R.layout.solar_object_item, parent, false)
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.solar_object_item, parent, false)
         return SolarObjectsViewHolder(itemView, this)
     }
 
